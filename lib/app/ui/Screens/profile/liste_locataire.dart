1f8c2156ec4ad3ff7c048eaa4a3fdf146970849b
Benjamin batundi <benbatundi7@gmail.com>
2022-12-27 02:02:17 -0800

    done with add notification UI and squelleton

--- lib/app/ui/Screens/profile/liste_locataire.dart
+++ lib/app/ui/Screens/profile/liste_locataire.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class LIsteLocataire extends StatelessWidget {
+class LIsteLocataire extends StatefulWidget {
   const LIsteLocataire({super.key});
 
   @override
+  State<LIsteLocataire> createState() => _LIsteLocataireState();
+}
+
+class _LIsteLocataireState extends State<LIsteLocataire> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "liste locatire",
-        style: TextStyle(color: Colors.black),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(
+            left: 30,
+            right: 30,
+            top: 10,
+            bottom: 50,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Liste locateiares",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              30.heightBox,
+              const Expanded(
+                child: SingleChildScrollView(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
