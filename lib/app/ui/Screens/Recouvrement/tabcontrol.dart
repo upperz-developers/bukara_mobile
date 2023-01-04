8c1c53505ba11c2a3d9299554c850a3ee571f695
Benjamin batundi <benbatundi7@gmail.com>
2023-01-04 04:32:53 -0800

    refactoring ui of check mail and profil

---
+++ lib/app/ui/Screens/Recouvrement/tabcontrol.dart
@@ -0,0 +1,86 @@
+import 'package:bukara/app/ui/screens/Recouvrement/recouvrement.dart';
+import 'package:flutter/material.dart';
+
+import '../../shared/style.dart';
+
+class TopTabBar extends StatefulWidget {
+  const TopTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<TopTabBar> createState() => _TopTabBarState();
+}
+
+class _TopTabBarState extends State<TopTabBar>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabControllertop;
+  @override
+  void initState() {
+    super.initState();
+    _tabControllertop = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabControllertop!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TabBar(
+              isScrollable: true,
+              padding: const EdgeInsets.only(
+                bottom: 0,
+                top: 15,
+              ),
+              controller: _tabControllertop,
+              unselectedLabelColor: AppColors.SECOND_TEXT_COLOR,
+              indicator: const UnderlineTabIndicator(
+                  borderSide: BorderSide(width: 0.0)),
+              //indicatorColor: Colors.white,
+              indicatorWeight: 5.0,
+              labelColor: AppColors.BLACK_COLOR,
+              tabs: const [
+                Tab(
+                  child: Text(
+                    "Recouvrement",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Paiement",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ]),
+          Expanded(
+            child: TabBarView(
+                physics: const BouncingScrollPhysics(),
+                controller: _tabControllertop,
+                children: const [
+                  Recouvrement(),
+                  Center(
+                    child: Text(
+                      "historique de paiement",
+                      style: TextStyle(
+                        color: AppColors.BLACK_COLOR,
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/app/ui/Screens/app_page.dart
+++ lib/app/ui/Screens/app_page.dart
@@ -1,4 +1,5 @@
 import 'package:bukara/app/ui/Screens/profile/profile.dart';
+import 'package:bukara/app/ui/screens/Recouvrement/tabcontrol.dart';
 import 'package:bukara/app/ui/screens/home/home_page.dart';
 import 'package:bukara/app/ui/screens/locataire/liste_locataire.dart';
 import 'package:flutter/material.dart';
@@ -147,88 +148,6 @@ class _AppPage extends State<AppPage> with SingleTickerProviderStateMixin {
                 ),
               ),
             ),
-        ],
-      ),
-    );
-  }
-}
-
-class TopTabBar extends StatefulWidget {
-  const TopTabBar({Key? key}) : super(key: key);
-
-  @override
-  State<TopTabBar> createState() => _TopTabBarState();
-}
-
-class _TopTabBarState extends State<TopTabBar>
-    with SingleTickerProviderStateMixin {
-  TabController? _tabControllertop;
-  @override
-  void initState() {
-    super.initState();
-    _tabControllertop = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _tabControllertop!.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TabBar(
-              isScrollable: true,
-              padding: const EdgeInsets.only(
-                bottom: 0,
-                top: 15,
-              ),
-              controller: _tabControllertop,
-              unselectedLabelColor: AppColors.SECOND_TEXT_COLOR,
-              indicator: const UnderlineTabIndicator(
-                  borderSide: BorderSide(width: 0.0)),
-              //indicatorColor: Colors.white,
-              indicatorWeight: 5.0,
-              labelColor: AppColors.BLACK_COLOR,
-              tabs: const [
-                Tab(
-                  child: Text(
-                    "Recouvrement",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    "Paiement",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ]),
-          Expanded(
-            child: TabBarView(
-                physics: const BouncingScrollPhysics(),
-                controller: _tabControllertop,
-                children: const [
-                  Recouvrement(),
-                  Center(
-                    child: Text(
-                      "historique de paiement",
-                      style: TextStyle(
-                        color: AppColors.BLACK_COLOR,
-                      ),
-                    ),
-                  ),
-                ]),
-          ),
         ],
       ),
     );
