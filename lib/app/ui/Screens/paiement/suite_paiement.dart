16e5cb94047a19c21694e81e513f540a73d4e1ab
Benjamin batundi <benbatundi7@gmail.com>
2023-01-10 01:14:41 -0800

    recouvrement par appartement

--- lib/app/ui/Screens/paiement/suite_paiement.dart
+++ lib/app/ui/Screens/paiement/suite_paiement.dart
@@ -1,10 +1,7 @@
 import 'package:bukara/app/ui/shared/style.dart';
-import 'package:bukara/app/ui/shared/utils/utility%20fonction/utility.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
-
 import '../../../providers/paiement_modele/modele.dart';
-import '../../shared/utils/utility fonction/customer_date.dart';
 import '../../shared/utils/widget.dart';
 
 class SuitePaiement extends StatelessWidget {
